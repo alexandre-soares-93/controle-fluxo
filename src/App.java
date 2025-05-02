436ca2e6b6654aba77a7ded7eb675f213c2259a7
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-05-02 17:46:04 -0300

    Aula sobre condicionais compostas

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class App {
+    public static void main(String[] args) {
+
+
+    }
 }
--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class CaixaEletronico {
+
+    public static void main(String[] args) {
+
+        double saldo  = 25.0;
+        double valorSolicitado = 17.0;
+
+        if(valorSolicitado < saldo) {
+            saldo -= valorSolicitado;
+            System.out.println("Novo saldo: R$" + saldo);
+        } else {
+            System.out.println("Saldo Insuficiente");
+        }
+
+    }
 }
